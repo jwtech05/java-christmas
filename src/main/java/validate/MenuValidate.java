6116a97f1213517f1c42d86c7ef39cccb3b9a1db
jwAhn1020 <takartin@gmail.com>
2023-11-15 14:41:34 +0900

    feat : 메뉴는 한 번에 최대 20개까지만 제한 하는 기능 구현 완료

--- src/main/java/validate/MenuValidate.java
+++ src/main/java/validate/MenuValidate.java
@@ -22,21 +22,9 @@ public class MenuValidate {
         singleMenuNumValidate(orderMenu);
         existValidate(orderMenu);
         onlyDrinkBeveragesMenuValidate(orderMenu);
+        limitTotalMenuNumValidate(orderMenu);
 
         this.orderMenu = orderMenu;
-    }
-
-    // 이벤트 주의 사항 - 음료만 주문시 에러 발생
-    private void onlyDrinkBeveragesMenuValidate(String orderMenu){
-        Map<String, Integer> inputOrders = InputMenuOrganize(orderMenu);
-        List<String> orderNames = new ArrayList<>(inputOrders.keySet());
-        int checkMenu = 0;
-        for (String orderName : orderNames) {
-            if ((menu.getAppetizers().containsKey(orderName) || menu.getMains().containsKey(orderName) || menu.getDesserts().containsKey(orderName))){
-                checkMenu++;
-            }
-        }
-        if(checkMenu == 0) throw new IllegalArgumentException("[ERROR] 이벤트 주의 사항 - 음료만 주문 시, 주문할 수 없습니다.");
     }
 
     //메뉴 형식과 다른경우 에러 발생
@@ -51,7 +39,7 @@ public class MenuValidate {
 
     //메뉴 중복이 있을 경우 에러 발생
     private void duplicateValidate(String orderMenu) {
-        Map<String, Integer> inputOrders = InputMenuOrganize(orderMenu);
+        Map<String, Integer> inputOrders = inputMenuOrganize(orderMenu);
         List<String> orderNames = new ArrayList<>(inputOrders.keySet());
         for (int i = 0; i < orderNames.size() - 1; i++) {
             for (int j = i + 1; j < orderNames.size(); j++) {
@@ -64,7 +52,7 @@ public class MenuValidate {
 
     //한 메뉴의 개수가 0이거나 그 이하일 경우 에러 발생
     private void singleMenuNumValidate(String orderMenu) {
-        Map<String, Integer> inputOrders = InputMenuOrganize(orderMenu);
+        Map<String, Integer> inputOrders = inputMenuOrganize(orderMenu);
         List<Integer> orderQuantity = new ArrayList<>(inputOrders.values());
         for (int i = 0; i < orderQuantity.size(); i++) {
             int quantity = orderQuantity.get(i);
@@ -76,13 +64,36 @@ public class MenuValidate {
 
     //메뉴에 존재하지 않는 메뉴 일시 에러 발생
     private void existValidate(String orderMenu) {
-        Map<String, Integer> inputOrders = InputMenuOrganize(orderMenu);
+        Map<String, Integer> inputOrders = inputMenuOrganize(orderMenu);
         List<String> orderNames = new ArrayList<>(inputOrders.keySet());
         for (String orderName : orderNames) {
             if (!checkMenusContain(orderName)) {
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
             }
         }
+    }
+
+    // 이벤트 주의 사항 - 음료만 주문시 에러 발생
+    private void onlyDrinkBeveragesMenuValidate(String orderMenu){
+        Map<String, Integer> inputOrders = inputMenuOrganize(orderMenu);
+        List<String> orderNames = new ArrayList<>(inputOrders.keySet());
+        int checkMenu = 0;
+        for (String orderName : orderNames) {
+            if ((menu.getAppetizers().containsKey(orderName) || menu.getMains().containsKey(orderName) || menu.getDesserts().containsKey(orderName))){
+                checkMenu++;
+            }
+        }
+        if(checkMenu == 0) throw new IllegalArgumentException("[ERROR] 이벤트 주의 사항 - 음료만 주문 시, 주문할 수 없습니다.");
+    }
+
+    private void limitTotalMenuNumValidate(String orderMenu){
+        Map<String, Integer> inputOrders = inputMenuOrganize(orderMenu);
+        List<Integer> orderQuantity = new ArrayList<>(inputOrders.values());
+        int quantity = 0;
+        for (int i = 0; i < orderQuantity.size(); i++) {
+            quantity += orderQuantity.get(i);
+        }
+        if(quantity > 20) throw new IllegalArgumentException("[ERROR] 이벤트 주의 사항 - 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
     }
 
     private boolean checkMenusContain(String orderName) {
@@ -95,7 +106,7 @@ public class MenuValidate {
         return false;
     }
 
-    public Map<String, Integer> InputMenuOrganize(String menu) {
+    private Map<String, Integer> inputMenuOrganize(String menu) {
         Map<String, Integer> menuOrganize = new HashMap<>();
         String[] menuItems = menu.split(",");
 
